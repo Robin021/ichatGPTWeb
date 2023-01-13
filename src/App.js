656a9b86dc4e25fe951649151d624faf91e2ffd7
robin021 <westman021@qq.com>
2023-01-13 20:10:07 +0800

    change style and add notify after sending message

--- src/App.js
+++ src/App.js
@@ -1,41 +1,75 @@
 import React from 'react';
 import { useEffect, useState } from 'react';
 import './App.css';
+import { Button } from 'antd';
+import { AliwangwangOutlined } from '@ant-design/icons';
+import { Alert } from 'antd';
+import { Input, Space } from 'antd';
+
 
 function App() {
+  const [error, setError] = useState(false);
   const [questions, setQuestions] = useState([]);
   const [answers, setAnswers] = useState([]);
   const [prevAnswer, setPrevAnswer] = useState('');
-  const [conversationId, setConversationId] = useState(''); 
+  const [conversationId, setConversationId] = useState('');
+  const [connectionError, setConnectionError] = useState(false);
   const inputRef = React.useRef();
+  const { Search } = Input;
+  const [searchValue, setSearchValue] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+
+  const suffix = (
+    <AliwangwangOutlined
+      style={{
+        fontSize: 16,
+        color: '#1890ff',
+      }}
+    />
+  );
   const handleSubmit = (e) => {
-    e.preventDefault();
-    setQuestions([...questions, e.target.elements.question.value]);
-    inputRef.current.value = '';
+    setQuestions([...questions, e]);
+    // console.log(e)
+    setSearchValue('');
+    setIsLoading(true);
   };
-
-
-
-  useEffect(() => {
-    const getAnswer = async () => {
-      if (questions.length === 0) {
-        return;
-      }
-      let response = await fetch(`http://chatgpt.021d.com:5001/ask?q=${questions[0]}&conversation_id=${conversationId}`); 
+  const getAnswer = async () => {
+    if (questions.length === 0) {
+      return;
+    }
+    try {
+      setIsLoading(true);
+      let response = await fetch(`http://chatgpt.021d.com:5001/ask?q=${questions[0]}&conversation_id=${conversationId}`);
       response = await response.json();
       setAnswers([...answers, response.answers]);
-      setQuestions([]);  
+      setQuestions([]);
       setPrevAnswer(prevAnswer + '\n你: ' + questions[0] + '\n\nChatBot:' + response.answers + '\n');
-    
-    };
+      setError(false);
+      setIsLoading(false);
+
+    } catch (e) {
+      setError(true);
+      setIsLoading(false);
+    }
+  }
+
+  useEffect(() => {
     getAnswer();
   }, [questions]);
   useEffect(() => {
+    inputRef.current.value = '';
+  }, []);
+
+  useEffect(() => {
     const getNewConversationId = async () => {
-      let response = await fetch(`http://chatgpt.021d.com:5001/new-conversation`);
-      response = await response.json();
-      const id = response.id;
-      setConversationId(id); 
+      try {
+        let response = await fetch(`http://chatgpt.021d.com:5001/new-conversation`);
+        response = await response.json();
+        const id = response.id;
+        setConversationId(id);
+      } catch (e) {
+        setConnectionError(true);
+      }
     };
     getNewConversationId();
   }, []);
@@ -44,16 +78,33 @@ function App() {
   }, [prevAnswer]);
 
   return (
+
     <div className='container'>
+      {connectionError && <div className="error-message">没有连上大脑，请刷下再产生会话</div>}
       <pre className='answer-area'>
         {prevAnswer}
         <br />
       </pre>
-      <form onSubmit={handleSubmit} className='question-form'>
-        <input name='question' ref={inputRef} type='text' />
-        <input type='submit' value='问' />
-      </form>
+      <Space direction="vertical">
+        <div className='alert-container'>
+          {isLoading && <Alert message="等待回复中..." type="info" />}
+        </div>
+        <Search
+          placeholder="跟我聊两句吧"
+          enterButton="Send"
+          size="large"
+          ref={inputRef}
+          suffix={suffix}
+          onSearch={handleSubmit}
+          value={searchValue}
+          onChange={e => setSearchValue(e.target.value)}
+        />
+      </Space>
+      {/* <Alert message="消息已发送" type="success" /> */}
+      {isLoading && <div>等待回复中...</div>}
+      {error && <Button color="danger" onClick={() => getAnswer()}>重新产生回答</Button>}
     </div>
+
   );
 }
 
