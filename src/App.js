800065b94580ce92f6c43d1605dfc5bed8c02e34
robin021 <westman021@qq.com>
2023-02-01 17:17:09 +0800

    change backend service

--- src/App.js
+++ src/App.js
@@ -2,24 +2,21 @@ import React from 'react';
 import { useEffect, useState } from 'react';
 import './App.css';
 import { WechatOutlined ,LoadingOutlined} from '@ant-design/icons';
-import { Input, Space,Spin,Button,Watermark,message } from 'antd';
+import { Input, Space,Spin,Watermark,message } from 'antd';
 
 
 
 function App() {
-  const [error, setError] = useState(false);
+  // const [error, setError] = useState(false);
   const [questions, setQuestions] = useState([]);
   const [answers, setAnswers] = useState([]);
   const [prevAnswer, setPrevAnswer] = useState('');
-  const [conversationId, setConversationId] = useState('');
-  const [connectionError, setConnectionError] = useState(false);
   const inputRef = React.useRef();
   const { Search } = Input;
   const [searchValue, setSearchValue] = useState('');
   const [isLoading, setIsLoading] = useState(false);
-  const DOMAIN = process.env.REACT_APP_DOMAIN;
   const [messageApi, contextHolder] = message.useMessage();
-   
+  const DOMAIN = process.env.REACT_APP_DOMAIN;
   const suffix = (
     <WechatOutlined
       style={{
@@ -43,18 +40,18 @@ function App() {
     }
     try {
       setIsLoading(true);
-      let response = await fetch(`http://${DOMAIN}:5001/ask?q=${questions[0]}&conversation_id=${conversationId}`);
+      let response = await fetch(`http://${DOMAIN}:5001/chat?q=${questions[0]}`);
       response = await response.json();
       setAnswers([...answers, response.answers]);
       setQuestions([]);
       setPrevAnswer(prevAnswer + '<div class="answer"><div class="answer-text">' + response.answers + '</div></div>');
-      setError(false);
+      // setError(false);
       setIsLoading(false);
     } catch (e) {
-      setError(true);
+      // setError(true);
       messageApi.open({
         type: 'info',
-        content: '请重新发送',
+        content: '大脑连接错误，请重试',
         maxCount: 1,
         duration: 2
       });
@@ -70,19 +67,7 @@ function App() {
     inputRef.current.value = '';
   }, []);
 
-  useEffect(() => {
-    const getNewConversationId = async () => {
-      try {
-        let response = await fetch(`http://${DOMAIN}:5001/new-conversation`);
-        response = await response.json();
-        const id = response.id;
-        setConversationId(id);
-      } catch (e) {
-        setConnectionError(true);
-      }
-    };
-    getNewConversationId();
-  }, []);
+
   useEffect(() => {
     document.querySelector('.answer-area').scrollTop = document.querySelector('.answer-area').scrollHeight;
   }, [prevAnswer]);
