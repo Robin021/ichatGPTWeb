e72888fb718672a486a0edb674518fe79699893c
robin021 <westman021@qq.com>
2023-02-13 15:54:06 +0800

    change backend port to 80

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -7,7 +7,7 @@ module.exports = function (app) {
             // 👇️ make sure to update your target
             // target: 'http://127.0.0.1:5001',
             // for docker-compose plase use below
-            target: 'http://backend:5001',
+            target: 'http://backend',
             headers: {
                 "Connection": "keep-alive"
             },
